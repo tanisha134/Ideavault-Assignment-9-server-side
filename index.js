f1903ea590851b7eef795e9a4bec1ee256eb7b40
Tanisha <jannatulnurtanisha63@gmail.com>
2026-05-31 23:10:46 +0600

    Added code and condition in Index.js and also connected mongodb in backend

--- index.js
+++ index.js
@@ -1,62 +1,70 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-const port = process.env.PORT || 5000;
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+dotenv.config();
 
 const uri = process.env.MONGODB_URI;
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  },
+  }
 });
 
 async function run() {
   try {
+    
+    await client.connect();
 
-    const ideasCollection = client
-      .db("ideaVaultDB")
-      .collection("ideas");
+    const db = client.db("idea-vault")
+    const ideaCollection = db.collection("ideas")
 
-    // POST IDEA
-    app.post("/ideas", async (req, res) => {
+    app.post('/addIdea', async (req, res) => {
+      
+      try{
 
-      const newIdea = req.body;
+      
+      const idea = req.body
+      console.log("Received:", idea);
+      const result = await ideaCollection.insertOne(idea)
 
-      const result = await ideasCollection.insertOne(newIdea);
+      res.send(result)
+    }catch (error) {
 
-      res.send(result);
-    });
+    console.log(error);
 
-    // GET IDEAS
-    app.get("/ideas", async (req, res) => {
-
-      const result = await ideasCollection.find().toArray();
-
-      res.send(result);
-    });
-
-    console.log("MongoDB Connected");
+    res.status(500).send({ error: error.message });
+    }
+  });
+  app.get('/ideas', async (req, res) => {
+    const result = await ideaCollection.find().toArray();
+    res.send(result);
+  });
+  
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
+    
+    // await client.close();
   }
 }
+run().catch(console.dir)
 
-run().catch(console.dir);
-
-app.get("/", (req, res) => {
-  res.send("IdeaVault Server Running");
+app.get('/', (req, res) => {
+    res.send('server is running totally fine!!!');
 });
 
-app.listen(port, () => {
-  console.log(`Server running on ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
